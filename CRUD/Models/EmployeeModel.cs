12a74c28688556110b0af2c44e0e079c022d6d49
manish <manish@drake.in>
2018-05-26 10:36:33 -0400

    First commit

---
+++ CRUD/Models/EmployeeModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD.Models
+{
+    public class EmployeeModel
+    {
+        public int EmployeeId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Number { get; set; }
+
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+
+        public int StateId { get; set; }
+
+        public string StateName { get; set; }
+
+    }
+}
